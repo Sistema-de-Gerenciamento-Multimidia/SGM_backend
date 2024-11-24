460f996d2e589cb741bb464294ad9c4b4aeca4d7
brunoestrella <brunogamer2002@gmail.com>
2024-11-24 18:28:28 -0300

    changing AUTH_USER_MODEL

--- app/settings.py
+++ app/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "authentication",
+    "users",
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

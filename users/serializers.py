be4dcefaa2836398be3d1ed6ab469c566268d967
brunoestrella <brunogamer2002@gmail.com>
2024-11-25 09:24:45 -0300

    creating users serializers

---
+++ users/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers  
+from users.models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        min_length=8,
+        max_length=16,
+        style={'input_type': 'password'}
+    )
+    password_confirmation = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password'}
+    )
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'name', 'password', 'password_confirmation')
+
+    def validate(self, data):
+        password = data.get('password')
+        password_confirmation = data.get('password_confirmation')
+
+        if password != password_confirmation:
+            raise serializers.ValidationError({'password_confirmation': "Passwords do not match."})
+        
+        return data
+    
+
+class UserListDetailSerializer(serializers.ModelSerializer):
+
+    class Meta():
+        model = User
+        fields = ['email', 'username', 'name']

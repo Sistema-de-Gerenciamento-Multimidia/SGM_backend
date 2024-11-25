32fde63eb4dd789f2fc8ea8269af5f740b983e5a
brunoestrella <brunogamer2002@gmail.com>
2024-11-24 23:19:39 -0300

    adding the custom user forms to admin

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,30 @@
 from django.contrib import admin
-from users.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+from .forms import UserCreationForm, UserChangeForm
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    # Formulários para adicionar e alterar usuários
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    # Campos exibidos no admin
+    list_display = ('email', 'username', 'name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'name', 'password')}),
+        ('Permissões', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+        ('Datas Importantes', {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('date_joined', 'last_login')
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'name', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'username', 'name')
+    ordering = ('email',)
+
+# Registro no admin
+admin.site.register(User, UserAdmin)

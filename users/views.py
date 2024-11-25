7262f1cabe7cca7f69afaf9a3cea6b35726f3f50
brunoestrella <brunogamer2002@gmail.com>
2024-11-25 09:25:00 -0300

    creating users views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, views, viewsets
+from users.serializers import UserListDetailSerializer
+from users.models import User
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserListView(viewsets.ViewSet):
+
+    def list(self, request):
+        queryset = User.objects.all()
+        serializer = UserListDetailSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = UserListDetailSerializer(user)
+        return Response(serializer.data)
+    
+    
+    def create(self, request):
+        ...
+
+
